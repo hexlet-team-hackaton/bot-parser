4b66de74ef572a3ce0a67d43d7f3693371ab21d9
Mikhail Orlov <crazyminer398@gmail.com>
2022-12-18 19:21:16 +0300

    created funcs for data work

--- bot_parser/src/data_work.py
+++ bot_parser/src/data_work.py
@@ -10,5 +10,12 @@ def get_model(user_request: dict) -> str:
     return user_request.get('model', None)
 
 
-def check_input_data(user_request: dict):
-    pass
+def check_input_data(user_request: dict) -> str:
+    if get_category(user_request) is None:
+        return 'Please enter correct category.'
+    elif get_brand(user_request) is None:
+        return 'Please enter correct brand.'
+    elif get_model(user_request) is None:
+        return 'Please enter correct model.'
+    else:
+        return 'Please waiting...'
